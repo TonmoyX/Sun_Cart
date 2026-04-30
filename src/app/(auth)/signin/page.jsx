71810e12d2d7bdacd4656d62a5dfcf4d9edbf82d
TonmoyX <142309932+TonmoyX@users.noreply.github.com>
2026-04-30 19:31:10 +0600

    signup and signin page setup

--- src/app/(auth)/signin/page.jsx
+++ src/app/(auth)/signin/page.jsx
@@ -1,9 +1,24 @@
+'use client'
+import Link from 'next/link';
 import React from 'react';
 
 const SignInPage = () => {
     return (
-        <div>
-            
+        <div className='container mx-auto justify-center mt-10'>
+            <form >
+            <fieldset className="fieldset bg-base-200 border-base-300 mx-auto mb-15 rounded-box w-xs border p-4">
+                <legend className="fieldset-legend text-lg">Login</legend>
+ 
+                <label className="label">Email</label>
+                <input type="email" name='email' className="input" placeholder="Email" />
+
+                <label className="label">Password</label>
+                <input type="password" name='password' className="input" placeholder="Password" />
+
+                <button className="btn btn-neutral mt-4">Login</button>
+            <p className='text-center text-lg'>You have no account ? <Link href={'/signup'}><span className='text-blue-600'>SignUp</span></Link></p>
+            </fieldset>
+            </form>
         </div>
     );
 };
--- src/app/(auth)/signup/page.jsx
+++ src/app/(auth)/signup/page.jsx
@@ -1,9 +1,26 @@
+import Link from 'next/link';
 import React from 'react';
 
 const SignUpPage = () => {
     return (
-        <div>
-            SignUp Page
+        <div className='container mx-auto justify-center mt-10'>
+            <form className=''>
+            <fieldset className="fieldset bg-base-200 border-base-300 mx-auto mb-15 rounded-box w-xs border p-4">
+                <legend className="fieldset-legend text-lg">Login</legend>
+ 
+                <label className="label">Name</label>
+                <input type="email" name='name' className="input" placeholder="Name" />
+ 
+                <label className="label">Email</label>
+                <input type="email" name='email' className="input" placeholder="Email" />
+
+                <label className="label">Password</label>
+                <input type="password" name='password' className="input" placeholder="Password" />
+
+                <button className="btn btn-neutral mt-4">Login</button>
+            <p className='text-center text-lg'>You have an account ? <Link href={'/signin'}><span className='text-blue-600'>SignIn</span></Link></p>
+            </fieldset>
+            </form>
         </div>
     );
 };
