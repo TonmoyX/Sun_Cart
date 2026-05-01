a87d41be70be96040dbc767f49a30958a68e45cb
TonmoyX <142309932+TonmoyX@users.noreply.github.com>
2026-05-01 23:00:03 +0600

    updated

--- src/app/(main)/product/page.jsx
+++ src/app/(main)/product/page.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ProductsCards from '@/component/products/ProductsCards';
 const ProductsPage = async () => {
-  const res = await fetch('http://localhost:3000/data.json');
+  const res = await fetch(`${process.env.BETTER_AUTH_URL}/data.json`);
     const data = await res.json();
     return (
        <ProductsCards data={data}></ProductsCards>
